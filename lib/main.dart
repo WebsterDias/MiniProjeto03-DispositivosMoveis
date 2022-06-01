3fa35a031317ca857391914a20da4ce39e82ecdc
Webster Dias Wolak <websterwolak@gmail.com>
2022-06-01 13:44:43 -0300

    commit do MiniProjeto03

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import '/home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Gasolina ou Etanol?",
+    home: Home(),
+  ));
+}
